43998c0c3f4ae11e81a853e763b867450eb9180d
Jessica Milewski <jlm323@pitt.edu>
2022-09-01 19:39:33 -0400

    light switch assignment

---
+++ lightswitch/script.js
@@ -0,0 +1,22 @@
+let buttonEl = document.querySelector("button");
+let htmlEl = document.querySelector("html");
+let h1EL = document.querySelector("h1")
+
+
+
+buttonEl.addEventListener('click', function(event) {
+    
+    htmlEl.style.backgroundColor = 'white';
+    h1EL.style.color = 'black';
+    
+})
+
+
+
+
+// buttonEl.addEventListener('click', function(event) {
+
+//  htmlEl.style.backgroundColor = 'black';
+//  h1EL.style.color = 'white';
+ 
+// })

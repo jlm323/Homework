f4686e4d7c5f8c52bf5bbba6c5cee79d5038dfb5
Jessica Milewski <jlm323@pitt.edu>
2022-09-04 18:00:41 -0400

    added ghost, styled button

--- lightswitch/script.js
+++ lightswitch/script.js
@@ -1,22 +1,29 @@
 let buttonEl = document.querySelector("button");
 let htmlEl = document.querySelector("html");
-let h1EL = document.querySelector("h1")
-
-
+let h1El = document.querySelector("h1");
+let theBulb = document.getElementById("lightBulb");
+let ghost = document.getElementById("ghost");
+let buttonColors = document.querySelector("on");
 
 buttonEl.addEventListener('click', function(event) {
     
-    htmlEl.style.backgroundColor = 'white';
-    h1EL.style.color = 'black';
+   
+    if (theBulb.src.match("https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png")) {
+
+			theBulb.src = "https://toppng.com/uploads/preview/light-bulb-png-transparent-light-bulb-115628766421gdl0skp2k.png";
+		    htmlEl.style.backgroundColor = 'white';
+            h1El.style.color = 'black';
+            buttonEl.innerText = 'SWITCH OFF';
+            ghost.style.visibility = 'hidden';
+
+
+    } else if (theBulb.src.match("https://toppng.com/uploads/preview/light-bulb-png-transparent-light-bulb-115628766421gdl0skp2k.png")) {
+
+            theBulb.src = "https://toppng.com/uploads/preview/light-bulb-on-off-png-11553940208oq66nq8jew.png";
+            htmlEl.style.backgroundColor = 'black';
+            h1El.style.color = 'white';
+            buttonEl.innerText = 'SWITCH ON';
+            ghost.style.visibility = 'visible';
+    };
     
 })
-
-
-
-
-// buttonEl.addEventListener('click', function(event) {
-
-//  htmlEl.style.backgroundColor = 'black';
-//  h1EL.style.color = 'white';
- 
-// })
